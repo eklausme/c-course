f6acf210fa30fa2db2f05168b0a3d58a8df910f6
Elmar Klausmeier <Elmar.Klausmeier@gmail.com>
2014-05-10 21:16:23 +0200

    Initial revision

--- ggt3.c
+++ ggt3.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
 int main (int argc, char *roman [2]) {
 	int unten=atoi(roman[1]);
 	int oben=atoi(roman[2]);
 	int x=oben;
-	int n=0;
 
 A:	if (unten%x==0) {
 		if (oben%x==0) {printf ("Der groesste gemeinsame Teiler von %d und %d lautet %d.\n", unten, oben, x);}
